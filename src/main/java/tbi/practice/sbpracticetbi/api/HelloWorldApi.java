3229a03d1f77317b0fd92f2305fb5c2375de32e5
stsuboi2 <shoko.tsuboi@unisys.co.jp>
2019-12-05 18:11:06 +0900

    create Hello World API

---
+++ src/main/java/tbi/practice/sbpracticetbi/api/HelloWorldApi.java
@@ -0,0 +1,15 @@
+package tbi.practice.sbpracticetbi.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldApi {
+
+    @GetMapping
+    public String getHello() {
+        return "Hello World";
+    }
+}
