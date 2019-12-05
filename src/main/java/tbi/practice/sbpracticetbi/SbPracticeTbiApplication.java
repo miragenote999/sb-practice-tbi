32c5b5dda972577bde3e871f64f5a8f40d3c8879
stsuboi2 <shoko.tsuboi@unisys.co.jp>
2019-12-05 15:36:10 +0900

    spring boot initializr

---
+++ src/main/java/tbi/practice/sbpracticetbi/SbPracticeTbiApplication.java
@@ -0,0 +1,13 @@
+package tbi.practice.sbpracticetbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbPracticeTbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbPracticeTbiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tbi/practice/sbpracticetbi/SbPracticeTbiApplicationTests.java
@@ -0,0 +1,13 @@
+package tbi.practice.sbpracticetbi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbPracticeTbiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
